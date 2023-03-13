f17adc055786656931e0087d7e56436bea24f39c
Robert Gripshover <robertgrips@gmail.com>
2023-03-13 13:42:18 -0400

    making my first commit on a few days

--- api/server.js
+++ api/server.js
@@ -4,9 +4,13 @@ const { logger } = require('./middleware/middleware')
 
 const server = express();
 
+const usersRouter = require('./users/users-router');
+
 server.use(express.json())
 
 server.use(logger)
+
+server.use('./api/users', usersRouter)
 
 // remember express by default cannot parse JSON in request bodies
 
