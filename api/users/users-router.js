50daa26473da17a38b49203bf08e907cf3403338
Robert Gripshover <robertgrips@gmail.com>
2023-03-13 14:31:59 -0400

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -7,33 +7,47 @@ const {
 
 } = require('../middleware/middleware')
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model')
+
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
-  console.log(req.user)
+router.get('/', (req, res, next) => {
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  console.log(req.user)
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+  res.json(req.user)
 });
 
-router.post('/', validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
-  console.log(req.name)
+router.post('/', validateUser, (req, res, next) => {
+  User.insert({name: req.name})
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-});
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, {name: req.name})
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
+}); //<<< This function is validating that the user ID entered is correct, 
+//then it is validating that the user is real, then it is taking in the id and the name object
+//and it is returning the ID of the one that was updating and 'then' sending back that user
+//with json
 
 router.delete('/:id', validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
@@ -51,6 +65,14 @@ router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
 });
+
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    customeMessage: 'something tragic inside posts router happend',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
 
