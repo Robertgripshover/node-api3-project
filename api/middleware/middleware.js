1d8c55bec23c88e563675a7a321e7aca45392dfa
Robert Gripshover <robertgrips@gmail.com>
2023-03-13 14:07:34 -0400

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -29,15 +29,27 @@ async function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUser middleware');
-  next();
+  const { name } = req.body
+  if(!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field",
+    })
+ } else {
+  req.name = name.trim() //<<< this is making the name not have any white space
+    next()
+ }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validatePost middleware');
-  next();
+  const { text } = req.body
+  if(!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field",
+    })
+ } else {
+  req.text = text.trim() //<<< this is making the name not have any white space
+    next()
+ }
 }
 
 module.exports = {
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const {
   validateUserId,
+  validateUser,
+  validatePost,
 
 } = require('../middleware/middleware')
 
@@ -12,19 +14,22 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  console.log(req.user)
 });
 
 router.get('/:id', validateUserId, (req, res) => {
+  console.log(req.user)
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.name)
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
@@ -38,9 +43,10 @@ router.delete('/:id', validateUserId, (req, res) => {
 router.get('/:id/posts', validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
+  console.log(req.user)
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
